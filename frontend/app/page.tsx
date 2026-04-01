d32afa439b9bd0c0121f8e038b5225e395fc8d07
Ha Duc Luong <haducluongdz@gmail.com>
2026-04-01 14:09:13 +0700

    ci/cd

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex items-center justify-center text-slate-500">
-      Đang tải...
+      Loading...
     </div>
   );
 }
